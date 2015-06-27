ae38a54e76fbadc75c080872706a652290384087
Shelby Manney <info@zone9.com>
2015-06-26 17:46:53 -0700

    Initial Commit

---
+++ dockittest3/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  dockittest3
+//
+//  Created by Shelby Manney on 6/26/15.
+//  Copyright (c) 2015 DockIt. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
